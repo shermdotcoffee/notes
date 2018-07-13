ef67d3bee467863de18a7786c73f33c58ce70acb
shermdotcoffee <shermmat@oregonstate.edu>
2018-07-12 22:40:52 -0400

    update

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-console.log("starting app")
+console.log('Starting app.js');
 
-const fs = require('fs');//loads the file system module
-const os = require('os');//loads the operating system module
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var res = notes.addNote();
-console.log(res);
-//var user = os.userInfo();
-//console.log(user);
-//fs.appendFileSync('greetings.txt',`Hello ${user.username}!, You are ${notes.age}.`);
-console.log('Result: ', notes.add(3,4));
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
+console.log('Yargs', argv);
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  notes.getNote(argv.title);
+} else if (command === 'remove') {
+  notes.removeNote(argv.title);
+} else {
+  console.log('Command not recognized');
+}
